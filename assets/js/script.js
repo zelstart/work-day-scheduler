2183d876c04363cfb0c454be82cbfa51a57990d5
zelstart <e.pomeroy95@gmail.com>
2023-08-02 17:45:09 -0400

    added some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,17 @@
 
 $(function () {
+  // displays the day of the week and date on the header
   $('#currentDay').text(dayjs().format('dddd // MM • DD • YYYY'));
-});
 
-
-
-// when the save button is clicked, data in the text area is saved to local storage
+  // when the save button is clicked, data in the text area is saved to local storage
   $('.saveBtn').click(function () {
     var task = $(this).siblings(".description").val(); // siblings of saveBtn clicked, with a class called description. will save value of text area
     var hour = $(this).parent().attr("id"); // looks for the parent of the saveBtn clicked, saves the id 
     localStorage.setItem(hour, task) // saves the contents of task and hour of saveBtn clicked to local storage
   })
 
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-
-function loadSave() {
-// getItem from each hour (if there is any value there) and set the text of the corresponding hour to the saved value
+  function loadSave() {
+    // getItem from each stored hour key (if there is any value there) and set the text of the corresponding hour to the saved value
     $("#hour-9 .description").text(localStorage.getItem('hour-9'))
     $("#hour-10 .description").text(localStorage.getItem('hour-10'))
     $("#hour-11 .description").text(localStorage.getItem('hour-11'))
@@ -30,43 +23,38 @@ function loadSave() {
     $("#hour-17 .description").text(localStorage.getItem('hour-17'))
   }
 
+  // color code the text area based on the time of day
+  function colorCoding() {
+    // for each element with the time-block class...
+    $('.time-block').each(function () {
+      // ... grab the number in the data attribute "hour" and call it blockHour
+      var blockHour = this.dataset.hour;
+      var currentHour = dayjs().hour();
 
+      // if the blockHour is the same as currentHour, add the class "present", remove "past" and "future"
+      if (currentHour == blockHour) {
+        $(this).children("textArea").addClass("present")
+        $(this).children("textArea").removeClass("past")
+        $(this).children("textArea").removeClass("future")
+      }
 
+      // if the blockHour is before the currentHour, add the class "past", remove "present" and "future"
+      if (currentHour > blockHour) {
+        $(this).children("textArea").addClass("past")
+        $(this).children("textArea").removeClass("present")
+        $(this).children("textArea").removeClass("future")
+      }
 
+      //if the blockHour is later than the currentHour, add the class "future", remove "present" and "past"
+      if (currentHour < blockHour) {
+        $(this).children("textArea").addClass("future")
+        $(this).children("textArea").removeClass("past")
+        $(this).children("textArea").removeClass("present")
+      }
+    })
+  }
 
-// color code the text area based on the time of day
-function colorCoding() {
-  // for each element with the time-block class...
-  $('.time-block').each(function () {
-    // ... grab the number in the data attribute "hour" and call it blockHour
-    var blockHour = this.dataset.hour;
-    var currentHour = dayjs().hour();
+  colorCoding();
+  loadSave()
 
-    // if the blockHour is the same as currentHour, add the class "present", remove "past" and "future"
-    if (currentHour == blockHour) {
-      $(this).children("textArea").addClass("present")
-      $(this).children("textArea").removeClass("past")
-      $(this).children("textArea").removeClass("future")
-    }
-
-    // if the blockHour is before the currentHour, add the class "past", remove "present" and "future"
-    if (currentHour > blockHour) {
-      $(this).children("textArea").addClass("past")
-      $(this).children("textArea").removeClass("present")
-      $(this).children("textArea").removeClass("future")
-    }
-
-    //if the blockHour is later than the currentHour, add the class "future", remove "present" and "past"
-    if (currentHour < blockHour) {
-      $(this).children("textArea").addClass("future")
-      $(this).children("textArea").removeClass("past")
-      $(this).children("textArea").removeClass("present")
-    }
-  })
-}
-
-
-
-colorCoding();
-loadSave()
-
+});
